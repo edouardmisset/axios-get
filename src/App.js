2a5a8265b83bde011e22b085afed824e6ef04e7e
Edouard Misset <edouardmisset@gmail.com>
2022-02-01 11:41:00 +0100

    Created the app / api call

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+
+const GITHUB_API_ENDPOINT = `https://api.github.com/search/repositories?q=stars:>1+language:javascript&sort=stars&order=desc&type=Repositories`
 
 function App() {
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState('')
+  const [data, setData] = useState(null)
+
+  async function fetchData() {
+    setLoading(true)
+    try {
+      const result = await axios.get(GITHUB_API_ENDPOINT)
+      console.log(result)
+      setData(result.data.items)
+      setLoading(false)
+      setError(false)
+    } catch (error) {
+      console.error('error: ', error)
+      setError(`${error}`)
+      setLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  if (loading) return <p>Loading ...</p>
+  if (error)
+    return (
+      <p>
+        There was an error loading the repos.{' '}
+        <button onClick={fetchData}>Try again</button>
+      </p>
+    )
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div>
+      <h1>Top Github repos</h1>
+      {data.map((repo, index) => (
+        <p key={repo.id}>
+          {index + 1}. <a href={repo.url}>{repo.name}</a> (
+          {repo.stargazers_count.toLocaleString()} ⭐️)
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
